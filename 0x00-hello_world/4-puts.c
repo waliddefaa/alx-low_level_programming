ded8b1c6a3703bf65f4b90fcc4a073f4c1cc95e0
walid <defaawalid@gmail.com>
2023-06-15 21:55:29 +0100

    hello!

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+
+/**
+ * main - starting point of the program
+ *
+ *
+ * Return: 0 ending point
+ *
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+		return (0);
+}
