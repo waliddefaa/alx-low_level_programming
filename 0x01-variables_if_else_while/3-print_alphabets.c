d2a702c3701cc03518f00845e6f69449125fd795
walid <defaawalid@gmail.com>
2023-06-17 00:03:10 +0100

    alphabet2

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Enrty point of the program
+ *
+ * Description:  a program that prints the alphabet in lowercase and uppercase
+ *
+ * Return: 0 ending point
+ */
+
+int main(void)
+
+{
+	int i = 97;
+	int z = 65;
+
+	while (n <= 122)
+	{
+		puthcar(i);
+		i++;
+	}
+
+	while (z <= 90)
+	{
+		putchar(z);
+		z++;
+	}
+
+	putchar('\n')
+
+	return (0);
+}
