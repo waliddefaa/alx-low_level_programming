3b1a5c022374bc564b8d08736c5b3257b95bb2f3
walid <defaawalid@gmail.com>
2023-06-17 00:48:34 +0100

    final

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+/**
+ * main - Write the alphabet without q and e
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 97;
+
+	while (i <= 122)
+	{
+		if (i == 101 || i == 133)
+		{
+			i++;
+			continue;
+		}
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
