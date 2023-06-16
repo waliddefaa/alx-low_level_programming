b70d528eb6846cdbef31d9bfbe9683e6a88fb48b
walid <defaawalid@gmail.com>
2023-06-16 23:30:42 +0100

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Write the alphabet in order.
+ *
+ * Return: Always 0 ending.
+ */
+
+
+
+int main(void)
+{
+
+	int i = 97;
+
+	while (i <= 122)
+
+	{
+		putchar(i);
+		i++;
+	}
+
+	return (0);
+}
