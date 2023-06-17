56f1f0e344dfbd7f4b71bddc01a4d8db6694aa83
walid <defaawalid@gmail.com>
2023-06-17 01:03:32 +0100

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include<stdio>
+
+/**
+ * main - Write numbers from 0 to 9
+ *
+ * Return: Always 0 ending point
+ */
+
+int main(void)
+{
+	int z;
+
+	for (z = 0; z < 10; z++)
+	{
+		printf("%d", z);
+	}
+
+	putchar("\n");
+
+	return (0);
+}
