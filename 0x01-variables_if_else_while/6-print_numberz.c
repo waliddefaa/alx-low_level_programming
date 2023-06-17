74325d1565ea6105cc611c48b9cfaa6ae821d3a1
walid <defaawalid@gmail.com>
2023-06-17 01:17:02 +0100

    nums

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'this program print all single digit numbers'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int z;
+
+	for (z = 48; z < 58; z++)
+	{
+		putchar(z);
+	}
+	putchar('\n');
+
+	return (0);
+}
