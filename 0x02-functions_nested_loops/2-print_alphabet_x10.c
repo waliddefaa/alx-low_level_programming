7dd3e161538ec0818d9f37b18cef28d5debe9a77
walid <defaawalid@gmail.com>
2023-06-20 20:33:24 +0100

    third

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include"main.h"
+
+/**
+ * print_alphabet_x10 - Write the alphabet 10 times
+ *
+ * Return: nothing.
+ */
+void print_alphabet_x10(void);
+{
+	char a;
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
