70918deb80b45a3e47249ccbd65c87855548a2ae
walid <defaawalid@gmail.com>
2023-06-29 22:43:29 +0100

    task7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -11,25 +11,25 @@
 
 char *leet(char *s)
 {
-	int count = 0, i;
+	int x = 0, i;
 	int low_letters[] = {97, 101, 111, 116, 108};
 	int upp_letters[] = {65, 69, 79, 84, 76};
 	int numbers[] = {52, 51, 48, 55, 49};
 
 
-	while (*(s + count) != '\0')
+	while (*(s + x) != '\0')
 	{
 
 		for (i = 0; i < 5; i++)
 		{
 
-			if (*(s + count) == low_letters[i] || *(s + count) == upp_letters[i])
+			if (*(s + x) == low_letters[i] || *(s + x) == upp_letters[i])
 			{
-				*(s + count) = numbers[i];
+				*(s + x) = numbers[i];
 				break;
 			}
 		}
-		count++;
+		x++;
 	}
 
 
