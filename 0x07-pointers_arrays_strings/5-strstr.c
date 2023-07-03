e79132de5580f74cf01c57c8b324606787537188
walid <defaawalid@gmail.com>
2023-07-03 23:05:22 +0100

    task5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr - 
+ * @haystack: 
+ * @needle: 
+ * Return: 
+ */
+
+
+
+
+
+
+char  *_strstr(char *haystack, char *needle)
+{
+	int a;
+
+	if (*needle == 0)
+		return (haystack);
+
+
+	while (*haystack)
+	{
+		a = 0;
+
+		if (haystack[a] == needle[a])
+		{
+			do {
+				if (needle[a + 1] == '\0')
+					return (haystack);
+				a++;
+			} while (haystack[a] == needle[a]);
+		}
+		haystack++;
+	}
+	return ('\0');
+}
