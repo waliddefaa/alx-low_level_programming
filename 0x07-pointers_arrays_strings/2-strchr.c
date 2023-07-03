9accb39ca13dd7096e1606dbeaa297d3e80bc582
walid <defaawalid@gmail.com>
2023-07-03 22:41:59 +0100

    task2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *  _strchr - locates a character in a string
+ *  @s: input
+ *  @c: input
+ *  Return: pointer to the first occurrence
+ */
+
+
+
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	while (1)
+	{
+
+		i = *s++;
+		if (i == c)
+		{
+			return (s - 1);
+		}
+		if (i == 0)
+		{
+			return (NULL);
+		}
+	}
+}
