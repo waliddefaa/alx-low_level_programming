aab24c7c402fc9ce593dec5a3c557e6c35d622ba
walid <defaawalid@gmail.com>
2023-07-13 23:35:33 +0100

    task1

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,7 +1,7 @@
 #ifndef main_h
 #define main_h
 
-
+#include<stdlib.h>
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
